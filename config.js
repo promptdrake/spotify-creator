acccd0f1f2b150a7c270a7dd0c830424cbde01db
Aisbircubes <95160973+promptdrake@users.noreply.github.com>
2023-10-19 22:28:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+ "domain": "aisbircubes.my.id",
+ "password": "awikwok1234",
+ "bot_token": "",
+ "owner_chatid": "", // ketik /id ke @mindpastebot - https://t.me/mindpastebot jan lupa /start di bot tujuanmu
+ "is_using_number_on_email": true // tambahan 2 nomer ke email kamu (kami menyarankan true supaya menghindari email telah terpakai)
+}
---
+++ index.js
@@ -0,0 +1,175 @@
+const puppeteer = require('puppeteer-extra');
+const axios = require('axios');
+const chalk = require('chalk');
+const config = require('./config');
+const userAgents = require('user-agents');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const userAgentsList = Array.from({ length: 10 }, () => new userAgents());
+  puppeteer.use(StealthPlugin());
+  async function createSpotifyAccount() {
+    const response = await axios.get('https://randomuser.me/api/?nat=us');
+    console.log('[Server] Fetching Random User api');
+    const userData = response.data.results[0];
+    const username = userData.name.last;
+    console.log('[Server] Name Found ' + username);
+    const browser = await puppeteer.launch({
+      headless: "new",
+      args: [
+        /*`--user-agent=${userAgentsList[Math.floor(Math.random() * userAgentsList.length)].toString()}`,*/
+        `--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36`,
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins,site-per-process'/*,
+        `--load-extension=${path.resolve(__dirname, 'captcha')}`*/ // Replace with the actual path to your CRX file
+      ],
+    });
+    const page = await browser.newPage();
+    await page.setExtraHTTPHeaders({
+      'Accept-Language': 'en'
+    });
+    await page.setViewport({ width: 1080, height: 1024 });
+    const userAgent = await page.evaluate(() => {
+      return navigator.userAgent;
+    });
+    
+    console.log(chalk.green(userAgent));
+    const enterEmailAndSubmit = async (email) => {
+      console.log(chalk.blue('[Server] ') + 'Email: ' + email);
+      await page.type('input#username', email);
+      await page.waitForTimeout(2000); 
+      await page.click('button[data-testid="submit"]');
+    };
+  
+    console.log('[Server] Opening Browser...');
+    await page.goto('https://www.spotify.com/id-id/signup', { waitUntil: 'domcontentloaded' });
+    console.log('[Server] Visiting Spotify Signup');
+  
+    await page.waitForSelector('input#username');
+    let email;
+    if (config.is_using_number_on_email) {
+      const randomNumbers = Math.floor(Math.random() * 100);
+      const formattedNumbers = String(randomNumbers).padStart(2, '0');
+      email = username + formattedNumbers + '@' + config.domain;
+    } else {
+      email = username + '@' + config.domain;
+    }
+    enterEmailAndSubmit(email);
+    const errorElement = await page.$('.Text-sc-g5kv67-0.hfclbh');
+    if (errorElement) {
+      console.log('Error found. Deleting and retrying email.');
+      await page.evaluate(() => {
+        document.querySelector('input#username').value = '';
+      });
+      enterEmailAndSubmit(email);
+    }
+  
+    await page.waitForSelector('input#new-password');
+    console.log(chalk.blue('[Server] ') +'Password: ' + config.password);
+    await page.type('input#new-password', config.password);
+    await page.waitForTimeout(2000);
+    await page.click('button[data-testid="submit"]');
+    await page.waitForSelector('input#displayName');
+    await page.type('input#displayName', username);
+    await page.waitForSelector('input#day');
+    console.log(chalk.blue('[Server] ') +'Adding Day 1');
+    await page.type('input#day', '1');
+    await page.select('#month', '1')
+    console.log(chalk.blue('[Server] ') +'Adding month 1');
+    await page.waitForSelector('input#year');
+    console.log(chalk.blue('[Server] ') +'Adding year 2000');
+    await page.type('input#year', '2000');
+    await page.click('input#gender_option_prefer_not_to_say');
+    console.log(chalk.blue('[Server] ') +'Gender: prefer not say');
+    await page.waitForTimeout(2000);
+    await page.click('button[data-testid="submit"]');
+    console.log(chalk.blue('[Server] ') +'Submiting Identity...');
+    await page.waitForTimeout(2000);
+    await page.click('button[data-testid="submit"]');
+    console.log(chalk.blue('[Server] ') +'Submiting tos...');
+    console.log(chalk.red('[Server] ') +'Security Checking Please wait a sec...');
+    await page.waitForTimeout(7000);
+    if (page.url().includes('challenge.spotify.com')) {
+      console.log(chalk.yellow('[Server] ') + 'CAPTCHA challenge detected, try restarting your ip address');
+browser.close()
+    } else {
+      const currentDate = new Date();
+const options = { day: 'numeric', month: 'long',year: 'numeric'  };
+const locale = 'id-ID';
+const date = currentDate.toLocaleDateString(locale, options);
+const ipaddress = await getUserIpAddress();
+  const region = await getLocationInfo(ipaddress);
+      console.log(chalk.green('[Server] ') + 'No CAPTCHA challenge detected. Saving credentials...');
+      const resultContent = `\n${email} | ${config.password}`;
+      fs.appendFileSync('result.txt', resultContent);
+        const message = `(+) New Account Spotify Created\nRegion: ${region}\n\nEmail: \`\`${email}\`\`\nPassword: \`\`${config.password}\`\`\n\nCreated in ${date}`;
+    
+        const response = await axios.post(
+          `https://api.telegram.org/bot${config.bot_token}/sendMessage`,
+          {
+            chat_id: config.owner_chatid,
+            text: message,
+            parse_mode: 'Markdown',
+          }
+        );
+    // .dev .bear
+      console.log(chalk.blue('[Server] ') + chalk.green('All done. The result is saving in result.txt'));
+      await browser.close();
+  console.log(chalk.blue('[Server] ') + 'Browser closed.');
+    }
+  }
+  async function getUserIpAddress() {
+    try {
+      const response = await axios.get('https://api.ipify.org');
+      return response.data.trim();
+    } catch (error) {
+      console.error('Error retrieving IP address:', error);
+      return 'Unknown';
+    }
+  }
+  
+async function askUserCount() {
+    const ipaddress = await getUserIpAddress();
+  const location = await getLocationInfo(ipaddress);
+    const text = `
+  ░█▀▀▀█ █▀▀█ █▀▀█ ▀▀█▀▀ ─▀─ █▀▀ █──█ 　 ░█▀▀█ █▀▀█ ▀▀█▀▀ 　 ─█▀▀█ █▀▀ █▀▀ █▀▀█ █──█ █▀▀▄ ▀▀█▀▀ 
+  ─▀▀▀▄▄ █──█ █──█ ──█── ▀█▀ █▀▀ █▄▄█ 　 ░█▀▀▄ █──█ ──█── 　 ░█▄▄█ █── █── █──█ █──█ █──█ ──█── 
+  ░█▄▄▄█ █▀▀▀ ▀▀▀▀ ──▀── ▀▀▀ ▀── ▄▄▄█ 　 ░█▄▄█ ▀▀▀▀ ──▀── 　 ░█─░█ ▀▀▀ ▀▀▀ ▀▀▀▀ ─▀▀▀ ▀──▀ ──▀── 
+  `;
+  console.log(chalk.red(text));
+  console.log('Created By @aisbircubes coder\nFollow Us\nTelegram: https://t.me/penyukaberuang\nInstagram: https://instagram.com/aisbircubes\n')
+  console.log(chalk.blue("(-) IP ADDRESS: ") + chalk.yellow(ipaddress))
+  console.log(chalk.green("(-) Location: ") + chalk.yellow(location))
+  console.log(chalk.cyan('Please use vpn for all region account want to create\nThis script is beta featured'))
+    rl.question(chalk.yellow('\nPress Ctrl+c to exit\n===================\n(+) ') + 'How many Spotify accounts do you want to create?\n- ', async (count) => {
+      count = parseInt(count);
+      if (isNaN(count) || count < 1) {
+        console.log('Please Enter Number Only!');
+        askUserCount();
+      } else {
+        puppeteer.use(StealthPlugin());
+        for (let i = 0; i < count; i++) {
+          console.log(chalk.green(`(+) Creating Spotify Account ${i + 1}`));
+          await createSpotifyAccount();
+          console.log(chalk.green(`(+) Succsesfully Creating Spotify Account ${i + 1}`));
+        }
+     askUserCount();
+      }
+    });
+  }
+  async function getLocationInfo(ipAddress) {
+    const apiUrl = `https://ipapi.co/${ipAddress}/json/`;
+    try {
+      const response = await axios.get(apiUrl);
+      return response.data.country_name;
+    } catch (error) {
+      console.error('Error retrieving location information:', error);
+      return 'oof'; 
+    }
+  }
+  askUserCount();
